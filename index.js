07909ad478207ea3df1a72724b1c5969373ca44e
butler-yun <myung5722@naver.com>
2021-02-05 16:42:51 +0900

    my first server and handle routes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello world!')
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`✅ Listening On: http://localhost:${PORT}`);
+}
+
+const home = (req, res) => {
+    res.send(`Welcome to my Home!`);
+}
+
+app.get('/', home);
+
+app.listen(PORT, handleListening);
