91e0c67afa765ab096c1e3a0729ae6ad2039f609
butler-yun <myung5722@naver.com>
2021-03-02 21:47:19 +0900

    Express middlewares

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+
 const app = express();
 
 const PORT = 4000;
@@ -10,6 +15,12 @@ const handleListening = () => {
 const home = (req, res) => {
     res.send(`Welcome to my Home!`);
 }
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan('dev'));
 
 app.get('/', home);
 
